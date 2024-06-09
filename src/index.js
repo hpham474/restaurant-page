5418ff2b9c5796cf4a8d345018ff1f44a877e57f
hpham474 <hpham474@gmail.com>
2024-06-09 02:49:27 -0500

    add event listeners to nav buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+const homeButton = document.querySelector(".home");
+const menuButton = document.querySelector(".menu");
+const contactButton = document.querySelector(".contact");
+const container = document.getElementById("content");
+
+homeButton.addEventListener("click", () => {
+    console.log("home");
+});
+menuButton.addEventListener("click", () => {
+    console.log("menu");
+});
+contactButton.addEventListener("click", () => {
+    console.log("contact");
+});
